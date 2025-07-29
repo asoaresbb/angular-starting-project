8e5d8c907ba17f6460e7bc2ad0f5e220a9b1ab19
asoaresb <asoares.csc@gmail.com>
2025-07-29 18:41:59 +0100

    add date to new task by default

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
-type User = {
-  id: string;
-  avatar: string;
-  name: string;
-};
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -14,6 +9,7 @@ type User = {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
